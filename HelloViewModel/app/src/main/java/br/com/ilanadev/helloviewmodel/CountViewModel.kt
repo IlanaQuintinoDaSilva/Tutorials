2fc3007498b52054343804ef2b6dddcd81452bc1
Ilana Quintino da Silva <ilana_silva@hotmail.com>
2018-07-28 11:26:20 -0300

    passagem count para CountViewModel

---
+++ HelloViewModel/app/src/main/java/br/com/ilanadev/helloviewmodel/CountViewModel.kt
@@ -0,0 +1,8 @@
+package br.com.ilanadev.helloviewmodel
+
+import android.arch.lifecycle.ViewModel
+
+class CountViewModel: ViewModel() {
+
+    var count = 0
+}
--- HelloViewModel/app/src/main/java/br/com/ilanadev/helloviewmodel/MainActivity.kt
+++ HelloViewModel/app/src/main/java/br/com/ilanadev/helloviewmodel/MainActivity.kt
@@ -1,23 +1,41 @@
 package br.com.ilanadev.helloviewmodel
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var model: CountViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        model = ViewModelProviders.of(this).get(CountViewModel::class.java)
+
+        setCount(model.count)
+
         btCount.setOnClickListener { onClickCount() }
     }
 
     private fun onClickCount() {
-        val frag
-                = supportFragmentManager.findFragmentById(R.id.HelloFragment) as HelloFragment
-        frag.incrementar()
+        // Incrementar
+        val s = tCount.text.toString()
+        var i = 0
+        i = if ("" == s) {
+            0
+        } else {
+            Integer.parseInt(s)
+        }
+        setCount(++i)
     }
 
-
+    private fun setCount(i: Int) {
+        model.count = i
+        tCount.text = (i).toString()
+        Log.d("up", "count $i")
+    }
 }
