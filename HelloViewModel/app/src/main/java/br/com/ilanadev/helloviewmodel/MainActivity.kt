68502316d9ca61558241eab89eb1ab8e389bbf20
Ilana Quintino da Silva <ilana_silva@hotmail.com>
2018-07-28 10:29:32 -0300

    fragment

--- HelloViewModel/app/src/main/java/br/com/ilanadev/helloviewmodel/MainActivity.kt
+++ HelloViewModel/app/src/main/java/br/com/ilanadev/helloviewmodel/MainActivity.kt
@@ -7,27 +7,45 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btCount.setOnClickListener { onClickCount() }
+
+        if(savedInstanceState != null) {
+            count = savedInstanceState.getInt("count")
+            setCount(count)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putInt("count",count)
     }
 
     private fun onClickCount() {
-        // Incrementar
-        val s = tCount.text.toString()
-        var i = 0
-        i = if ("" == s) {
-            0
-        } else {
-            Integer.parseInt(s)
-        }
-        setCount(++i)
+//        // Incrementar
+//        val s = tCount.text.toString()
+//        var i = 0
+//        i = if ("" == s) {
+//            0
+//        } else {
+//            Integer.parseInt(s)
+//        }
+//        setCount(++i)
     }
 
     private fun setCount(i: Int) {
-        tCount.text = (i).toString()
+        this.count = i
+        //tCount.text = (i).toString()
         Log.d("up", "count $i")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
