0ae5d5b13a1d1dcb9a1f16f52de0fbf890fe92e2
Ilana Quintino da Silva <ilana_silva@hotmail.com>
2018-07-28 11:06:46 -0300

    incrementando com fragment

--- HelloViewModel/app/src/main/java/br/com/ilanadev/helloviewmodel/MainActivity.kt
+++ HelloViewModel/app/src/main/java/br/com/ilanadev/helloviewmodel/MainActivity.kt
@@ -2,50 +2,22 @@ package br.com.ilanadev.helloviewmodel
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btCount.setOnClickListener { onClickCount() }
-
-        if(savedInstanceState != null) {
-            count = savedInstanceState.getInt("count")
-            setCount(count)
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-
-        outState?.putInt("count",count)
     }
 
     private fun onClickCount() {
-//        // Incrementar
-//        val s = tCount.text.toString()
-//        var i = 0
-//        i = if ("" == s) {
-//            0
-//        } else {
-//            Integer.parseInt(s)
-//        }
-//        setCount(++i)
+        val frag
+                = supportFragmentManager.findFragmentById(R.id.HelloFragment) as HelloFragment
+        frag.incrementar()
     }
 
-    private fun setCount(i: Int) {
-        this.count = i
-        //tCount.text = (i).toString()
-        Log.d("up", "count $i")
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-    }
 }
